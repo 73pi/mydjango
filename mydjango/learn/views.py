687ae2e581d29d178c44c17e5243a5ad943f2187
73pi <18852855260@163.com>
2019-02-17 17:02:43 +0800

    数据库和表单

--- mydjango/learn/views.py
+++ mydjango/learn/views.py
@@ -5,10 +5,12 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 
 
+# ^first/
 def index(request):
     return HttpResponse(u"欢迎来到我的django学习  welcome my first response!")
 
 
+# ^add/$
 def add(request):
     a = request.GET['a']
     b = request.GET['b']
@@ -16,15 +18,18 @@ def add(request):
     return HttpResponse(str(c))
 
 
+# ^new_add/(\d+)/(\d+)/$
 def add2(requset, a, b):
     c = int(a)+int(b)
     return HttpResponse(str(c))
 
 
+# ^first_template/
 def frist_template(requset):
     return render(requset, 'home.html')
 
 
+# ^add/(\d+)/(\d+)/$
 def old_too_new_url(request, a, b):
     # 实现templates中的home.html的<a>URL是旧的也可以跳转到对应name的url
     # reverse接收url中的name作为第一个参数，我们在代码中就可以通过reverse()来获取对应的网址
@@ -32,6 +37,7 @@ def old_too_new_url(request, a, b):
     return HttpResponseRedirect(reverse('add2', args=(a, b)))
 
 
+# ^template_adv/
 def template_adv(request):
     arg = {}
     arg['string'] = "这是字符串展示！"
@@ -40,5 +46,6 @@ def template_adv(request):
     return render(request, 'template-adv.html', arg)
 
 # 表单
+# r'^form/'
 def form(request):
     return render(request, 'form.html')
